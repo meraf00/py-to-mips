48ab109fc464a41c7fcd1a70b72842f9292fce9e
meraf00 <meraf000@gmail.com>
2023-02-16 04:50:35 -0800

    Added print

--- compiler.py
+++ compiler.py
@@ -1,26 +1,29 @@
 pycode = """
 x = 3
-if x:
+if x == 2:
     x = 4
     while x < 10:
         x = x + 1
     print(2)
 print(x)
 """
-def typ(char):
-     if char.isalpha():
-        tp="str"
-     elif char.isdigit():
-        tp = "num"
-     else:
-        tp = "sym"
-     return tp
 
-def match_pattern(line):
+
+def typ(char):
+    if char.isalpha():
+        tp = "str"
+    elif char.isdigit():
+        tp = "num"
+    else:
+        tp = "sym"
+    return tp
+
+
+def tokenize(line):
     line = line.strip()
     token = []
     itera = enumerate(line)
-    i =0
+    i = 0
     while i < len(line):
         char = line[i]
         if char in ["'", '"']:
@@ -29,43 +32,73 @@ def match_pattern(line):
                     break
             else:
                 raise Exception("unterminated string >" + line)
-            
+
             token.append(line[i:j+1])
         elif char in [" ", "\t"]:
             continue
         else:
-            cur =[char]
-            t=typ(char)
-            
+            cur = []
+            t = typ(char)
+
             while i < len(line):
                 char = line[i]
                 if char in [" ", "\n", "\t"] or typ(char) != t:
-                    i += 1
-                    break                
+                    break
                 cur.append(char)
-                i += 1            
-                
+                i += 1
+
             cur = "".join(cur)
             if cur.isnumeric():
-                token.append(float(cur))
+                token.append(int(cur))
             else:
                 token.append(cur)
         i += 1
     return token
-    
+
+
 class Block:
     def __init__(self):
         self.child = []
+
     def __repr__(self):
         return str(self.child)
-  
+
     def recreate(self, depth=0):
         for child in self.child:
             if isinstance(child, str):
                 yield (depth, child)
             else:
                 yield from child.recreate(depth + 1)
-    
+
+    def print_int(self, num):
+        return f"li $a0, {num}\nli $v0, 1\nsyscall"
+
+    def input(self, dest):
+        return f"li $a0, {dest}\n\nli $v0, 1\nsyscall"
+
+    def to_mips(self, python_code):
+        tokens = tokenize(python_code)
+        ins_type = match_pattern(tokens)
+
+        if ins_type == "PRINT":
+            arg = tokens[1]
+            if isinstance(arg, int):
+                return self.print_int(arg)
+
+        elif ins_type == "ASSIGN":
+            arg = tokens[1]
+            if isinstance(arg, int):
+                return self.input(arg)
+
+        return ""
+
+    def compile(self):
+        for child in self.child:
+            if isinstance(child, str):
+                yield self.to_mips(child)
+            else:
+                yield from child.compile()
+
     def walk(self):
         for child in self.child:
             if isinstance(child, str):
@@ -73,8 +106,11 @@ class Block:
             else:
                 yield from child.walk()
 
+
 def isblank(line):
-    return len(line.strip() ) ==0       
+    return len(line.strip()) == 0
+
+
 def indent(line):
     cnt = 0
     for char in line:
@@ -84,33 +120,87 @@ def indent(line):
             break
     return cnt
 
+
+def is_assignment(tokens):
+    if len(tokens) >= 3 and tokens[1] == "=":
+        return True
+
+
+def is_conditional(tokens):
+    if len(tokens) >= 1 and tokens[0] == "if":
+        return True
+
+
+def is_for_loop(tokens):
+    if len(tokens) >= 1 and tokens[0] == "for":
+        return True
+
+
+def is_while_loop(tokens):
+    if len(tokens) >= 1 and tokens[0] == "while":
+        return True
+
+
+def is_print(tokens):
+    if len(tokens) >= 1 and tokens[0] == "print":
+        return True
+
+
+def is_input(tokens):
+    if len(tokens) >= 1 and tokens[0] == "input":
+        return True
+
+
+def match_pattern(tokens):
+    if is_assignment(tokens):
+        return "ASSIGN"
+    elif is_conditional(tokens):
+        return "CONDITIONAL"
+    elif is_for_loop(tokens):
+        return "FOR"
+    elif is_while_loop(tokens):
+        return "WHILE"
+    elif is_print(tokens):
+        return "PRINT"
+    elif is_input(tokens):
+        return "INPUT"
+
+
 blocks = [Block()]
 
-prev = 0            
+prev = 0
 for line in pycode.split("\n"):
     print(line)
     if isblank(line):
         continue
     cur = blocks[-1]
-    i =   indent(line)
+    i = indent(line)
     if i == prev:
         cur.child.append(line.strip())
-        
+
     elif i > prev:
         new = Block()
         new.child.append(line.strip())
         cur.child.append(new)
         blocks.append(new)
     else:
-        for _ in range( (prev - i) // 4):
+        for _ in range((prev - i) // 4):
             blocks.pop()
         cur = blocks[-1]
-        
+
         cur.child.append(line.strip())
     prev = i
-print(blocks)        
+
+print(blocks)
 
 for ins in blocks[0].walk():
-    print(ins)
-    print(match_pattern(ins))
-    print()
+    tokens = tokenize(ins)
+    ins_type = match_pattern(tokens)
+    print(ins, tokens, ins_type)
+
+mips_code = []
+for mips in blocks[0].compile():
+    mips_code.append(mips)
+
+with open("mips.asm", "w") as f:
+    f.write("\n\n".join(mips_code))
